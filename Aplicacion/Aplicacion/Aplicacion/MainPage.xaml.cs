85889befb36d64508486975ca30c18c996abe435
Oscar Gutierrez Garcia <oskiyas94@hotmail.es>
2020-04-04 16:23:14 +0200

    Creación de la primera página XAML

---
+++ Aplicacion/Aplicacion/Aplicacion/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Aplicacion
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
+/** 
+ * 1. ¿Qué extensión de marcado de Xamarin.Forms permite establecer una propiedad XAML en un valor estático que se define en una clase de código subyacente?
+ * x:Static
+ * 
+ * 2. En XAML, {x:Static} y {x:Null} son ejemplos de ________.
+ * Extensiones de marcado
+ * 
+ * 3. ¿Qué interfaz usa para crear una extensión de marcado personalizada?
+ * IMarkupExtension
+ * 
+**/
