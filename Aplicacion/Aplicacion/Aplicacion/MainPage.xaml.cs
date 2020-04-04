59b127e1f427076b4b1622623c76ea607d0f4175
Oscar Gutierrez Garcia <oskiyas94@hotmail.es>
2020-04-04 16:46:52 +0200

    Adición de comportamiento a la página XAML

--- Aplicacion/Aplicacion/Aplicacion/MainPage.xaml.cs
+++ Aplicacion/Aplicacion/Aplicacion/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,58 @@ namespace Aplicacion
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
+        string telefono;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+
+        void OnTranslate(object sender, EventArgs e)
+        {
+            string telefono_introducido = txt_telefono.Text;
+            telefono = Translator.ToNumber(telefono_introducido);
+
+            if (!string.IsNullOrEmpty(telefono))
+            {
+                btn_llamar.IsEnabled = true;
+                btn_llamar.Text = "Llamar " + telefono;
+            }
+            else
+            {
+                btn_llamar.IsEnabled = false;
+                btn_llamar.Text = "Llamar";
+            }
+        }
+
+
+        async void OnCall(object sender, System.EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                "Introduzca un número",
+                "¿Llamar a " + telefono + "?",
+                "Si",
+                "No"))
+            {
+                try
+                {
+                    PhoneDialer.Open(telefono);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Imposible comunicar", "Número de teléfono no válido.", "OK");
+                }
+                catch (FeatureNotSupportedException)
+                {
+                    await DisplayAlert("Imposible comunicar", "Comunicacion no soportada.", "OK");
+                }
+                catch (Exception)
+                {
+                    // Other error has occurred.
+                    await DisplayAlert("Imposible comunicar", "Comunicación fallida.", "OK");
+                }
+            }
         }
     }
 }
---
+++ Aplicacion/Aplicacion/Aplicacion/Resources.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Aplicacion
+{
+    public static class Resources
+    {
+        public static Color Color_Fondo_Boton
+        {
+            get { return Color.FromRgb(0xff, 0xa5, 0); }
+        }
+
+        public static Color Color_Texto_Boton
+        {
+            get { return Color.White; }
+        }
+    }
+}
